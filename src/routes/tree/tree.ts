aa3c107c174b3c1267d2ade8d1ca1102cc2f2e82
Svathna <s.s.vathna@gmail.com>
2020-01-13 11:21:17 +0700

    filter tree by user

--- src/routes/tree/tree.ts
+++ src/routes/tree/tree.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 const mongoose = require('mongoose');
 import requires from '../../middleware/requires';
 const _ = require('lodash');
-import { TreeModel } from '../../models';
+import { TreeModel, UserModel } from '../../models';
 const { validationResult } = require('express-validator/check');
 const cloudinary = require('cloudinary').v2;
 const upload = require('../../config/multer');
@@ -41,6 +41,29 @@ app.get('/:id', async (req, res) => {
     }
     // send the user back
     return res.json({ tree, success: true, message: 'Success' });
+  } catch (error) {
+    return res.json({ success: false, error });
+  }
+});
+
+/**
+ * GET: Get tree by user `/tree/user/:id`
+ */
+app.get('/user/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await UserModel.findOne({ _id: id, deleted: false });
+    const allTrees = await TreeModel.find({ deleted: false });
+    // filter tree by user
+    const trees = allTrees.filter(tree => {
+      return tree.owner == id;
+    });
+    // sanity check for user
+    if (trees.length === 0) {
+      return res.status(400).json({ success: false, message: 'User never plant any tree' });
+    }
+    // send the user back
+    return res.json({ trees, success: true, message: 'Success' });
   } catch (error) {
     return res.json({ success: false, error });
   }
